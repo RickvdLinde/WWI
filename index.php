ab688334dba05b08776878972034aa1bd9308d7a
Jorrik24 <44639981+Jorrik24@users.noreply.github.com>
2018-10-31 13:59:40 +0100

    index.php

--- index.php
+++ index.php
@@ -1,21 +1,38 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>World Wide Importers</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
-    <form method="get" action="search.php">
+    <body>
+        <h1 class="logo">World Wide Importers</h1>
+        <div>
+            <ul class="header">
+                <li>Winkelwagen</li>
+                <li><a href="inloggen.php">Inloggen</a></li>
+            </ul>
+        </div>
+        <form method="get" action="search.php">
         Zoeken: <input type="text" name="zoekresultaat">
         <input type="submit" value="Zoeken" name="Zoeken"></form>
-    <body>
-        <?php
-  
+    <?php
+        $db = "mysql:host=localhost;dbname=wideworldimporters;port=3306";
+        $user = "root";
+        $pass = "";
+        $pdo = new PDO($db, $user, $pass);
         
+        $stmt = $pdo->prepare("SELECT * FROM customercategories");
+        $stmt->execute();
+        
+        print("<div class=\"navbar\"><ul>");
+        while($row = $stmt->fetch()){
+            $category = $row["CustomerCategoryName"];
+            print("<li><a href=\"$category.php\">" . $category . "</a></li>");
+        }
+        print("</ul></div>");
+        
+        $pdo = NULL;
         ?>
     </body>
 </html>
