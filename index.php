3ad3e49c238deed4dbfc7aa81cbe5b96025ef01a
Jorrik24 <44639981+Jorrik24@users.noreply.github.com>
2018-11-02 14:17:44 +0100

    qwerty

--- index.php
+++ index.php
@@ -1,38 +1,16 @@
+<?php
+include "functions.php"
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>World Wide Importers</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="Mainstyle.css">
     </head>
     <body>
-        <h1 class="logo">World Wide Importers</h1>
-        <div>
-            <ul class="header">
-                <li>Winkelwagen</li>
-                <li><a href="inloggen.php">Inloggen</a></li>
-            </ul>
-        </div>
-        <form method="post" action="search.php">
-        Zoeken: <input type="text" name="zoekresultaat" value="">
-        <input type="submit" value="Zoeken" name="Zoeken"></form>
-    <?php
-        $db = "mysql:host=localhost;dbname=wideworldimporters;port=3306";
-        $user = "root";
-        $pass = "";
-        $pdo = new PDO($db, $user, $pass);
-        
-        $stmt = $pdo->prepare("SELECT * FROM customercategories");
-        $stmt->execute();
-        
-        print("<div class=\"navbar\"><ul>");
-        while($row = $stmt->fetch()){
-            $category = $row["CustomerCategoryName"];
-            print("<li><a href=\"$category.php\">" . $category . "</a></li>");
-        }
-        print("</ul></div>");
-        
-        $pdo = NULL;
+        <?php
+        print(category());
         ?>
     </body>
 </html>
---
+++ inloggen.php
@@ -0,0 +1,21 @@
+<?php
+include "functions.php"
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>World Wide Importers</title>
+        <link rel="stylesheet" type="text/css" href="Mainstyle.css">
+    </head>
+    <body>
+       <?php
+       print(category());
+       ?>
+        <form>
+            <label for="user">Gebruikersnaam: </label><input type="text" id="user" name="user"><br>
+            <label for="pass">Wachtwoord: </label><input type="password" id="pass" name="pass"><br>
+            <input type="submit" value="Inloggen">
+        </form>
+    </body>
+</html>
