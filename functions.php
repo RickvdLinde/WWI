3eab66dcd38d02e4e73c527b676d0f40f5912c07
Avijn <38077967+Avijn@users.noreply.github.com>
2018-11-02 14:17:18 +0100

    .

--- functions.php
+++ functions.php
@@ -23,3 +23,42 @@ function zoeken($zoeken){
             }
 $pdo = NULL;
 }
+
+function category(){
+    print('<header>
+        <div class="kop">
+            <div class="logo">
+                <a href="index.php"><img src="Images/WWIlogo.png"></a>
+            </div>
+            <nav>
+                <a>Winkelwagen</a>
+                <a href="inloggen.php">Inloggen</a>
+            </nav>
+        </div>
+        </header>
+        <div class="category">');
+        
+        $db = "mysql:host=localhost;dbname=wideworldimporters;port=3306";
+        $user = "root";
+        $pass = "";
+        $pdo = new PDO($db, $user, $pass);
+        
+        $stmt = $pdo->prepare("SELECT * FROM stockgroups");
+        $stmt->execute();
+        
+        print("<div class=\"navbar\"><ul>");
+        while($row = $stmt->fetch()){
+            $category = $row["StockGroupName"];
+            print("<a href=\"$category.php\">" . $category . "</a>");
+        }
+        print("</ul></div>");
+        
+        $pdo = NULL;
+        
+        print('
+        <form method="POST" action="search.php" class="zoeken">
+            <input type="text" placeholder="Zoeken.." name="zoekresultaat">
+            <input type="submit" placeholder="Zoeken.."value="Zoeken" name="Zoeken">
+        </form>
+        </div>');
+}
