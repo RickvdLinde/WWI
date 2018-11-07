df88558c26f665e97fa9fe7c86a79cc1f27a186a
RickvdLinde <rick-vanderlinde@hotmail.com>
2018-11-07 11:33:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,5 +12,15 @@ include "functions.php"
         <?php
         print(category());
         ?>
+        
+        <h1 class="text">
+            Welkom op onze pagina!
+        </h1>
+        
+        <p class = "serrif">
+            Hier kunt u producten bekijken en bestellen.
+        </p>
+        
+        
     </body>
 </html>
