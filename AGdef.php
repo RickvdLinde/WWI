dbe5dc86c68fe6b9ac79038eac6fb20805b07afe
ThAYOVirus <bramherman9@gmail.com>
2018-11-07 11:44:43 +0100

    Update AGdef.php
    update AGdef

--- AGdef.php
+++ AGdef.php
@@ -12,8 +12,35 @@ and open the template in the editor.
     <body>
 <?php
   $AGs = filter_input(INPUT_GET,
-    "product", FILTER_SANITIZE_STRING);
-  print($AGs); //we kennen hem weer!! 
+    "productgroup", FILTER_SANITIZE_STRING);
+  //print($AGs); //we kennen hem weer!!
+  
+          // Verbinding maken database
+        $db ="mysql:host=localhost;dbname=wideworldimporters;port=3306";
+        $user = "root";
+        $pass = "";
+        $pdo = new PDO($db, $user, $pass);
+        
+        // voorbereiden query selecteren items van een categorie
+  $AGassign = filter_input(INPUT_GET, $AGs , FILTER_SANITIZE_STRING);
+        
+$stmt = $pdo->prepare("select * from stockitems SI join 
+    stockitemstockgroups SISG on SI.StockItemID=SISG.StockItemID 
+    join stockgroups SG on SISG.StockGroupID=SG.StockGroupID 
+    where StockItemName = ? order by StockItemName"); 
+
+// Uitvoeren query 
+$stmt->execute(array($AGassign));
+
+
+// Resultaten verwerken namen items
+while ($row = $stmt->fetch()) {
+
+	$CitemGroupName = $row["StockItemName"];
+        print($CitemGroupName);
+}
+// verbinding opruimen
+$pdo = NULL;        
 ?>
 
     </body>
