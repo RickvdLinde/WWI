a557565a62cf41779cb13d792d64fcecb0881d96
Tsnpfl <44642413+Tsnpfl@users.noreply.github.com>
2018-11-13 12:27:25 +0100

    Slideshow

--- index.php
+++ index.php
@@ -22,7 +22,22 @@ include "functions.php"
         <p class="tk border">
             Hier kunt u producten bekijken en bestellen
         </p>
-
+        <br>
+        <div class="slider-holder">
+        <span id="slider-image-1"></span>
+        <span id="slider-image-2"></span>
+        <span id="slider-image-3"></span>
+        <div class="image-holder">
+            <img src="Images/usb.jpg" class="slider-image" />
+            <img src="Images/usb.jpg" class="slider-image" />
+            <img src="Images/usb.jpg" class="slider-image" />
+        </div>
+        <div class="button-holder">
+            <a href="#slider-image-1" class="slider-change"></a>
+            <a href="#slider-image-2" class="slider-change"></a>
+            <a href="#slider-image-3" class="slider-change"></a>
+        </div>
+    </div>
         
     </body>
 </html>
