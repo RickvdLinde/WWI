e5f200f7e26f8f09b469f7ce042b4331a4ef8b27
RickvdLinde <rick-vanderlinde@hotmail.com>
2018-11-13 11:08:16 +0100

    Update Clothing.php

--- Clothing.php
+++ Clothing.php
@@ -98,8 +98,6 @@ while ($row = $stmt->fetch()) {
         	//print($CitemName . "<br>");
 } 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 ?>
         <div class="container">
 <?php
@@ -113,17 +111,15 @@ print("<li><a href=\"$IsAssignedAG7.php\">" . $IsAssignedAG7. "</a></li>");
 print("<li><a href=\"$IsAssignedAG8.php\">" . $IsAssignedAG8. "</a></li>");
 print("<li><a href=\"$IsAssignedAG9.php\">" . $IsAssignedAG9. "</a></li>");  
 print("<li><a href=\"$IsAssignedAG10.php\">" . $IsAssignedAG10. "</a></li>");  
-=======
->>>>>>> 767cd749fe9d360b2b3df60789408c7ee9584a89
+
 ?>
 <div class="container">
             
         <ul>
-=======
+
 ?>
 <div class="container">
             
->>>>>>> ebb37880915ab7e85427861881383be31c35ca1d
         <li><a href="AGdef.php?productgroup=<?php print($IsAssignedAG1);?>"><?php print($IsAssignedAG1);?></a></li>        
         <li><a href="AGdef.php?productgroup=<?php print($IsAssignedAG2);?>"><?php print($IsAssignedAG2);?></a></li>
         <li><a href="AGdef.php?productgroup=<?php print($IsAssignedAG3);?>"><?php print($IsAssignedAG3);?></a></li>        
@@ -135,10 +131,8 @@ print("<li><a href=\"$IsAssignedAG10.php\">" . $IsAssignedAG10. "</a></li>");
         <li><a href="AGdef.php?productgroup=<?php print($IsAssignedAG9);?>"><?php print($IsAssignedAG9);?></a></li>
         <li><a href="AGdef.php?productgroup=<?php print($IsAssignedAG10);?>"><?php print($IsAssignedAG10);?></a></li>        
         <li><a href="AGdef.php?productgroup=<?php print($IsAssignedAG11);?>"><?php print($IsAssignedAG11);?></a></li>
-<<<<<<< HEAD
+
         </ul>  
-=======
->>>>>>> ebb37880915ab7e85427861881383be31c35ca1d
            
 </div>
 <?php
