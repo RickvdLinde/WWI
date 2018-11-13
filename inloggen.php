01d763193c9dae954f6d9464e7557ac285dc5c90
RickvdLinde <rick-vanderlinde@hotmail.com>
2018-11-13 15:51:15 +0100

    Update inloggen.php

--- inloggen.php
+++ inloggen.php
@@ -16,7 +16,7 @@ include "functions.php"
         <form method="POST" class="inloggen">
             <label for="user">E-mailadres: </label><input type="text" id="user" name="user"><br>
             <label for="pass">Wachtwoord: </label><input type="password" id="pass" name="pass"><br>
-            <input class="inloggenknop" type="submit" value="Inloggenen" name="inloggenknop">
+            <input class="inloggenknop" type="submit" value="Inloggen" name="inloggenknop">
         </form>
     </body>
 </html>
