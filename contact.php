302ed30cd9e4a2097e5b3d8b62d2ff466a108312
RickvdLinde <rick-vanderlinde@hotmail.com>
2018-11-14 09:59:50 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -10,10 +10,34 @@ include "functions.php"
     </head>
     <body class="bodi">
         <?php
+        
         print(category());
+        
+        $db = "mysql:host=localhost;dbname=wideworldimporters;port=3306";
+        $user = "root";
+        $pass = "";
+        $pdo = new PDO($db, $user, $pass);
+        
+        $stmt = $pdo->prepare("SELECT SupplierName, PostalCityID, PhoneNumber, FaxNumber, WebsiteURL FROM suppliers");
+        $result = $stmt->execute();
+        $rows = $stmt->fetchAll(); // assuming $result == true
+        $n = count($rows);
+        
+        if($rows > 0){
+            echo "<table><tr><th>Naam leverancier</th><th>Postcode</th><th>Telefoonnummer</th><th>Fax</th><th>Website</th></tr>";
+            while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["SupplierName"]."</td><td>".$row["PostalCityID"]."<tr><td>".$row["PhoneNumber"]."</td><td>".$row["FaxNumber"]."<tr><td>".$row["WebsiteURL"]."";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+        
+        
+        
         ?>
         <h1 class="text">
-            Contact informatie
+            Contactgegevens leveranciers
         </h1>
         
         <p class="tk border">
