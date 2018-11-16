1ec2f193c070765f957e2175109d0e9ba101da52
JorrinKievit <43169049+JorrinKievit@users.noreply.github.com>
2018-11-16 11:28:38 +0100

    .

--- session.php
+++ session.php
@@ -1,13 +1,13 @@
 <?php
-if(!empty($_SESSION['PersonID']))
-{
-$session_PersonID=$_SESSION['PersonID'];
-include('functions.php');
-$userLogin= new userLogin();
-}
-if(empty($session_uid))
-{
-header("Location: $url");
-}
-?>
+include 'connect.php';
+session(start);
+$usercheck = $_SESSION['user_id'];
+if (isset($usercheck)):?>
+    <a href="loguit.php">Uitloggen</a>
+<?php else: ?>
+    <a href="inloggen.php">Inloggen</a>
+<?php endif; ?>
+
+
+
 
