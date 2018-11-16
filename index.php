c274e1e814915dfc9060094392dfaafd022643e3
Tsnpfl <44642413+Tsnpfl@users.noreply.github.com>
2018-11-16 11:44:44 +0100

    .

--- index.php
+++ index.php
@@ -13,31 +13,30 @@ session_start();
         <?php
         print(category());
         ?>
-
-        <h1 class="text">
         
         <h1 class="txet">
             Wide World Importers
         </h1>
-
+        
         <br/>
-
+        
         <p class="tk border">
             Welkom bij onze webshop!
         </p>
-
+        
         <br>
         <p class="tk border">
-        Exclusieve weekdeals
-            <br>
-                    <img src="Images/3 kg Courier post bag (White).JPG" class="deals">
-        <img src="Images/Packing knife with metal insert blade.JPG" class="deals">
-        <img src="Images/Red and white heavy urgent despatch tape.jpg" class="deals">
+        Exclusieve weekdeals,
         <br>
-        <a class="dealtext" href="product.php?product=3 kg Courier post bag (White) 300x190x95mm">3 kg Courier post bag (White)</a>
+        <a href="product.php?product=Small 9mm replacement blades 9mm" ><img src="Images/Small 9mm replacement blades.jpg" class="deals"></a>
+        <a href="product.php?product=Packing knife with metal insert blade (Yellow) 9mm"><img src="Images/Packing knife with metal insert blade.JPG" class="deals"></a>
+        <a  href="product.php?product=Red and white urgent despatch tape 48mmx75m"><img src="Images/Red and white heavy urgent despatch tape.jpg"class="deals"></a>
+        <br>
+        <a class="dealtext" href="product.php?product=Small 9mm replacement blades 9mm">Small 9mm replacement blades</a>
         <a class="dealtext" href="product.php?product=Packing knife with metal insert blade (Yellow) 9mm">Packing knife with metal insert blade (Yellow) 9mm</a>
-        <a class="dealtext" href="product.php?product=Red and white urgent despatch tape 48mmx75m">Red and white urgent despatch</a>    
+        <a class="dealtext" href="product.php?product=Red and white urgent despatch tape 48mmx75m">Red and white urgent despatch</a>   
+        <br>
         </p>
-
+      
     </body>
 </html>
