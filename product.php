2f82ef7a2393d76b42f1150011a05e840d6d7487
Avijn <38077967+Avijn@users.noreply.github.com>
2018-11-20 10:10:34 +0100

    favicon toegevoegd

--- product.php
+++ product.php
@@ -8,6 +8,7 @@ include "functions.php"
     <head>
         <meta charset="UTF-8">
         <title>Wide World Importers</title>
+        <link rel="icon" href="Images/archixl-logo.png">
         <link rel="stylesheet" type="text/css" href="Mainstyle.css">
         <link rel="stylesheet" type="text/css" href="style2.css">
     </head>
