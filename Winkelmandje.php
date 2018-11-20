ccf2a1fe5657e6fc9c4b32be732f6138c659d9e2
Jorrik24 <44639981+Jorrik24@users.noreply.github.com>
2018-11-20 13:29:03 +0100

    verwijderen

--- Winkelmandje.php
+++ Winkelmandje.php
@@ -17,45 +17,51 @@ include "functions.php"
         print("<div class=\"borderpagina\">");
 
         print("<h2>Producten in Winkelwagen</h2><br>");
-        
+
         //Gegevens ophalen uit de tabel
         if (isset($_SESSION["naam"]) && isset($_SESSION["winkelwagen"]) && isset($_SESSION["aantal"]) && ($_SESSION["aantal"] > 0)) {
             $naam = $_SESSION["naam"];
             $winkelwagen = $_SESSION["winkelwagen"];
             $aantal = $_SESSION["aantal"];
             $prijs = $_SESSION["prijs"];
-            $bedrag = $prijs * $aantal;
-            // Zet een product in de array $winkelwagen
-            $winkelwagen[$naam] = array($aantal, $bedrag);
 
-            /* print("<table class=\"tabel\">");
-              foreach ($winkelwagen as $key => $value) {
-              print("<tr><td>" . $key . "</td><td>" . $value . "</td><td> €" . $bedrag . "</td><td><button>Verwijderen</button></td></tr>");
-              $totaleBedrag = $totaleBedrag + $bedrag;
-              } */
-            
+            if (isset($_GET["verwijderen"])) {
+                $bedrag = null;
+                unset ($winkelwagen[$naam]);
+            } else {
+                if (isset($_GET["opslaan"])) {
+                    $quantity = filter_input(INPUT_GET, "quantity", FILTER_SANITIZE_STRING);
+                    $bedrag = $prijs * $quantity;
+                    $winkelwagen[$naam] = array($quantity, $bedrag);
+                } else {
+                    // Zet een product in de array $winkelwagen
+                    $bedrag = $prijs * $aantal;
+                    $winkelwagen[$naam] = array($aantal, $bedrag);
+                }
+            }
+
+            $nummer = 0;
             //Laat gegevens van het product zien: Naam, aantal, prijs
             $totaleBedrag = 0;
+            print("<form method=\"GET\" action=\"winkelmandje.php\">");
             foreach ($winkelwagen as $key => $value) {
                 print($key . " ");
-                if (is_array($value)) {
-                    print("aantal: " . $value[0] . " Prijs: €" . $value[1]);
+                if (is_array($value) || $value[0] > 0) {
+                    print("aantal: " . "<input type=\"text\" name=\"quantity\" value='" . $value[0] . "'>" . " Prijs: €" . $value[1] . "<button type=\"submit\" formmethod=\"GET\" name=\"verwijderen\">Verwijderen</button>");
                     print("<br>");
-                } else {
-                    print($value . "  <br>");
                 }
             }
-            
             // Berekent het totale bedrag
-            foreach ($winkelwagen as $key => $value){
-                if (is_array($value)){
-                $totaleBedrag += $value[1];
+            foreach ($winkelwagen as $value) {
+                if (is_array($value)) {
+                    $totaleBedrag += $value[1];
                 }
             }
 
-            print("</table>");
             print("<br>Totale bedrag: €" . $totaleBedrag . "<br><br>");
+            print("<input type=\"submit\" value=\"Wijzigingen opslaan\" name=\"opslaan\"></form>");
             print("<a href=\"betalen.php\" class=\"betaalbutton\" >Naar betaalpagina</a>");
+
             $_SESSION["bedrag"] = $bedrag;
             $_SESSION["winkelwagen"] = $winkelwagen;
             $_SESSION["totalebedrag"] = $totaleBedrag;
