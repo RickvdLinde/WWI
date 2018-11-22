f34033103607d0713fef7c5ffbbe505425ca64a7
Jorrik24 <44639981+Jorrik24@users.noreply.github.com>
2018-11-22 14:09:47 +0100

    revert revert

--- functions.php
+++ functions.php
@@ -52,6 +52,7 @@ function category() {
                 <a href="index.php"><img src="Images/WWIlogo.png"></a>
             </div>
             <nav>
+<<<<<<< HEAD
             <a href="Winkelmandje.php">Winkelwagen</a>');
 ?><?php
     if ($loggedin) {
@@ -61,12 +62,43 @@ function category() {
     }
 ?>
     <?php
+=======
+                <a href="Winkelmandje.php">Winkelwagen</a>');
+?>
+    <?php
+
+    if ($loggedin) {
+        print("<a href=\"inloggen.php\">Uitloggen</a>");
+        session_destroy();
+    } else {
+        print("<a href=\"inloggen.php\">Account</a>");
+    }
+    ?><?php
+>>>>>>> parent of c159f9d... Revert "Revert "Revert "Merge branch 'master' of https://github.com/RickvdLinde/WWI"""
 
     print('</nav>
             </div>
         </header>
         <div class="category">');
+<<<<<<< HEAD
     $pdo = NULL;
+=======
+        $pdo = NULL;
+        $db = "mysql:host=localhost;dbname=wideworldimporters;port=3306";
+        $user = "root";
+        $pass = "";
+        $pdo = new PDO($db, $user, $pass);
+        
+        $stmt = $pdo->prepare("SELECT * FROM stockgroups");
+        $stmt->execute();
+        
+        print("<div class=\"navbar\"><ul>");
+        while($row = $stmt->fetch()){
+            $category = $row["StockGroupName"];
+            //$Catget = 0;
+       print(" <div class='category'>");
+
+>>>>>>> parent of c159f9d... Revert "Revert "Revert "Merge branch 'master' of https://github.com/RickvdLinde/WWI"""
     $db = "mysql:host=localhost;dbname=wideworldimporters;port=3306";
     $user = "root";
     $pass = "";
@@ -76,6 +108,7 @@ function category() {
     $stmt->execute();
 
     print("<div class=\"navbar\"><ul>");
+<<<<<<< HEAD
     while($row = $stmt->fetch()){
     $category = $row["StockGroupName"];
     //$Catget = 0;
@@ -83,6 +116,15 @@ function category() {
     //print("<a href=\"$categorylink.php\">" . $category . "</a>");
     $urlsub = '<a href=Subcategorie.php?category=';
     print($urlsub . ($categorylink) . ">" .($category) . "</a>");
+=======
+    while ($row = $stmt->fetch()) {
+        $category = $row["StockGroupName"];
+        //$Catget = 0;
+        $categorylink = preg_replace('/\s+/', '_', $category);
+        //print("<a href=\"$categorylink.php\">" . $category . "</a>");
+        $urlsub = '<a href=Subcategorie.php?category=';
+        print($urlsub . ($categorylink) . ">" . ($category) . "</a>");
+>>>>>>> parent of c159f9d... Revert "Revert "Revert "Merge branch 'master' of https://github.com/RickvdLinde/WWI"""
     }
     print("</ul></div>");
 
@@ -97,11 +139,16 @@ function category() {
         </div>');
     }
 
+<<<<<<< HEAD
     function deals($deal2) {
+=======
+function deals() {
+>>>>>>> parent of c159f9d... Revert "Revert "Revert "Merge branch 'master' of https://github.com/RickvdLinde/WWI"""
     $db = "mysql:host=localhost;dbname=wideworldimporters;port=3306";
     $user = "root";
     $pass = "";
     $pdo = new PDO($db, $user, $pass);
+<<<<<<< HEAD
     $deal = $pdo->prepare("SELECT StockItemName FROM StockItems WHERE StockItemID LIKE ?");
     $deal->execute (array("$deal2"));
 
@@ -109,15 +156,53 @@ function category() {
     $item = $row["StockItemName"];
     print ($item);
     }
+=======
+    $deal = $pdo->prepare("SELECT StockItemName, RecommendedRetailPrice FROM StockItems WHERE StockItemName LIKE ?");
+    $deal2 = $pdo->prepare("SELECT StockItemName, RecommendedRetailPrice FROM StockItems WHERE StockItemName LIKE ?");
+    $deal3 = $pdo->prepare("SELECT StockItemName, RecommendedRetailPrice FROM StockItems WHERE StockItemName LIKE ?");
+    $deal->execute();
+    $deal2->execute();
+    $deal3->execute();
+
+    while ($row = $deal->fetch()) {
+        $item = $row["StockItemName"];
+        $prijs = $row["RecommededRetailPrice"];
+        print $item;
+        print (" " . $prijs);
+        print("<br>");
+    }
+    while ($row = $deal2->fetch()) {
+        $item2 = $row["StockItemName"];
+        $prijs2 = $row["RecommededRetailPrice"];
+        print $item2;
+        print (" " . $prijs2);
+        print("<br>");
+    }
+    while ($row = $deal3->fetch()) {
+        $item3 = $row["StockItemName"];
+        $prijs3 = $row["RecommededRetailPrice"];
+        print $item3;
+        print (" " . $prijs3);
+>>>>>>> parent of c159f9d... Revert "Revert "Revert "Merge branch 'master' of https://github.com/RickvdLinde/WWI"""
     }
 
+<<<<<<< HEAD
     function photo($photo2){
     $db = "mysql:host=localhost;dbname=wideworldimporters;port=3306";
+=======
+function footer() {
+    /* print("<footer><div><a href=\"info.php\">Over Wide World Importers</a>"
+      . "<a href=\"service.php\">Klantenservice</a><a href=\"leveranciers.php\">Leveranciers</a><a href=\"contact.php\">Contact</a></div></footer>"); */
+}
+function photo($photo2){
+        $db = "mysql:host=localhost;dbname=wideworldimporters;port=3306";
+>>>>>>> parent of c159f9d... Revert "Revert "Revert "Merge branch 'master' of https://github.com/RickvdLinde/WWI"""
     $user = "root";
     $pass = "";
     $pdo = new PDO($db, $user, $pass);
     $photo = $pdo->prepare("SELECT photo FROM StockItems WHERE StockItemID LIKE ?");
     $photo->execute (array("$photo2"));
+<<<<<<< HEAD
 
 
     while ($row = $photo->fetch()) {
@@ -134,3 +219,13 @@ function category() {
       . "<a href=\"service.php\">Klantenservice</a><a href=\"leveranciers.php\">Leveranciers</a><a href=\"contact.php\">Contact</a></div></footer>"); */
 
     }    
+=======
+    
+    
+     while ($row = $photo->fetch()) {
+        $item = $row["photo"];
+        print $item;
+     }
+}
+}
+>>>>>>> parent of c159f9d... Revert "Revert "Revert "Merge branch 'master' of https://github.com/RickvdLinde/WWI"""
