ef5fcb5483da6e9b6b6c3b3f72cedd8e65781f77
RickvdLinde <rick-vanderlinde@hotmail.com>
2018-11-30 12:12:32 +0100

    Update product.php

--- product.php
+++ product.php
@@ -30,34 +30,53 @@ include "functions.php"
             $comment = $row["MarketingComments"];
             $leverancier = $row["SupplierName"];
             ?>
-                    
-            <html>
-                <head>
-                    <meta charset="UTF-8">
-                    <title>Wide World Importers</title>
-                    <link rel="icon" href="Images/archixl-logo.png">
-                    <link rel="stylesheet" type="text/css" href="Mainstyle.css">
-                    <link rel="stylesheet" type="text/css" href="style2.css">
-                </head>
-                <body class="bodi">
 
-                <div class="slider-holder">
-                    <span id="slider-image-1"></span>
-                    <span id="slider-image-2"></span>
-                    <span id="slider-image-3"></span>
-                    <div class="image-holder">
-                        <img src="Images/1.jpg" class="slider-image" />
-                        <img src="Images/2.png" class="slider-image" />
-                        <img src="Images/3.jpg" class="slider-image" />
-                    </div>
-                    <div class="button-holder">
-                        <a href="#slider-image-1" class="slider-change"></a>
-                        <a href="#slider-image-2" class="slider-change"></a>
-                        <a href="#slider-image-3" class="slider-change"></a>
-                    </div>
-                </div>        
-        
-                    <?php
+
+            <head>
+            <meta charset="UTF-8">
+            <title>Wide World Importers</title>
+            <link rel="icon" href="Images/archixl-logo.png">
+            <link rel="stylesheet" type="text/css" href="Mainstyle.css">
+            <link rel="stylesheet" type="text/css" href="style2.css">
+            </head>
+            <body>
+
+            <div class="slideshow-container">
+
+            <div class="mySlides fade">
+                <img src="Images/1.png">
+            </div>
+
+            <div class="mySlides fade">
+                <img src="Images/2.png">
+            </div>
+
+            <div class="mySlides fade"> 
+                <img src="Images/3.png">
+            </div>
+
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+            </div>
+            <br>
+
+            <div style="text-align:center">
+              <span class="dot" onclick="currentSlide(1)"></span> 
+              <span class="dot" onclick="currentSlide(2)"></span> 
+              <span class="dot" onclick="currentSlide(3)"></span> 
+            </div>  
+            
+            <div class="formaantal">
+                <form method="get" action=Winkelmandje.php>
+                    <label for="aantal">Aantal Producten: </label><input type="number" id="aantal" name="aantal">
+                    <input class="toevoegenbutton" type="submit" name="submit" value="Toevoegen aan Winkelmandje">
+                </form>
+            </div>
+                    <?php print("<br><br><a href=\"leveranciers.php\" class=\"productleverancier\">Leverancier: " . $leverancier) . "</a>"; ?>
+                </div>
+                
+            <?php
                     print("<div class='productnaamprijs'><div class=\"productnaam\">" . $name . "</div>");
                     print("<div class=\"productprijs\">€" . $prijs) . "</div></div>";
                     if ($voorraad > 0) {
