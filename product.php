8e583cbcfaa08cbacd044c451aaeea5905756b1e
RickvdLinde <rick-vanderlinde@hotmail.com>
2018-12-04 12:31:15 +0100

    Product fix

--- product.php
+++ product.php
@@ -74,39 +74,6 @@ $keyresStock++;
 $keyresSupplier++;
         }
 
-        //als er een keuze is gemaakt uit de dropdownlist is deze if true, hij laadt het product zien met de value van de dropdownlist
-if(isset($_POST['small'])) {
-$dropdowncount = $_POST["small"];
-
-            ?>
-          <div class="productgegevens">
-                <div class="image-placeholder">
-
-                </div>
-                <div class="gegevenszonderafbeeling">
-                    <?php
-                    print("<div class=\"productnaam\">" . $itemresults[$dropdowncount] . "</div>");
-                    print("<div class=\"productprijs\">€" . $priceresults[$dropdowncount]) . "</div><br><br><br>";
-                    //print("<div class=\"productvoorraad\">Producten op voorraad: " . $Stockresults[$dropdowncount] . "<br><br>");
-                                if ($voorraad > 0) {
-                print('<div class="productopvoorraad">Product is op voorraad</div>');
-            } else {
-                print('<div class="productnietvoorraad">Product is niet op voorraad</div>');
-            }
-                    ?>
-                    <div class="formaantal">
-                        <form method="get" action=Winkelmandje.php>
-                            <label for="aantal">Aantal Producten: </label><input type="number" id="aantal" name="aantal">
-                            <input class="toevoegenbutton" type="submit" name="submit" value="Toevoegen aan Winkelmandje">
-                        </form>
-                    </div>
-                    <?php print("<br><br><a href=\"leveranciers.php\" class=\"productleverancier\">Leverancier: " . $Suplierresults[$dropdowncount]) . "</a>";
-                    ?>
-                </div>
-            </div>   
-<?php
-} 
-
 // om tot een dropdownlist te komen, moet er bepaald worden wat het verschil is tussen de categorienaam en de volledige naam. Dit gebeurt in de volgende code.         
 $arraynumitemres = array();
             $keyresnum = 0;
@@ -172,10 +139,6 @@ print('<form id="s" method="post">');
             }
                     ?>
                     <div class="formaantal">
-
-                        <form method="get" action=Winkelmandje.php>
-                            <label for="aantal">Aantal Producten: </label><input type="number" id="aantal" name="aantal">
-                            <input class="toevoegenbutton" type="submit" name="submit" value="Toevoegen aan Winkelmandje">
 
                         <form method="get" action="winkelmandje.php">
                             <label for="aantal">Number of products: </label><input type="number" id="aantal" name="aantal">
